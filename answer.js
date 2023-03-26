cbac5b92d30f3119f1c7f2f900db7989b491da28
mnubhan <76425959+mnubhan@users.noreply.github.com>
2023-03-26 23:15:58 +0100

    Create answer.js

---
+++ answer.js
@@ -0,0 +1,7 @@
+function openOrSenior(data){
+  return data.map(x => (x[0] >= 55) && (x[1] > 7) ? 'Senior' : 'Open')
+}
+
+function openOrSenior(data){
+  return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+}
